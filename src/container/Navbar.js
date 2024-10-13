582f245840bc6d62c3ff5657c335aa1d7a0eea89
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-10-13 21:38:11 +0530

    code pdated

--- src/container/Navbar.js
+++ src/container/Navbar.js
@@ -4,8 +4,8 @@ import "./Navbar.css";
 const Navbar = ({ theme, toggleTheme }) => {
   return (
     <div className="nav_container" style={{ backgroundColor: theme ? "#2a2828" : "#dddbff", color: theme ? "white" : "black" }}>
-        <div className="set_Title" ><img src="https://www.itspaydai.com/assets/logo-DnAxFD0z.svg" alt="paydai_img" />
-        <i>Paydai</i></div>
+        <div className="set_Title" >
+        <i>To Do List</i></div>
       <div></div>
       <div className="toggleBtn">
       <i class="ri-sun-fill"></i>
